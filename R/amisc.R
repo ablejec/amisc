57276ba4e5c822a36ecd1ecb5e4697783bc0a3bd
ablejec <andrej.blejec@nib.si>
2016-06-17 11:52:06 -0700

    FIrst few adds according to th Hadley's manual

---
+++ R/amisc.R
@@ -0,0 +1,12 @@
+#' amisc: A package with functions I find handy
+#'
+#' The package provides several functions I tend to use often.
+#'
+#' @section Debug functions:
+#'
+#' Debug functions type the result accordintg to the logical
+#' value of variable \code{.testing}
+#'
+#' @docType package
+#' @name amisc
+NULL
